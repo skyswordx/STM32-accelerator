94306bc351c4e164fb5931ddce1bc8a28cb0d99f
yuanyue <1140527828@qq.com>
2025-07-26 21:50:35 +0800

    更新正弦波时域检测施法

---
+++ Module/SineDetectTiming/my_sine_detect.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "my_sine_detect.h"
+
+
---
+++ Module/SineDetectTiming/my_sine_detect.h
@@ -0,0 +1,7 @@
+#ifndef MY_SINE_DETECT_H
+#define MY_SINE_DETECT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif // MY_SINE_DETECT_H
