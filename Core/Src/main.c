757272a7e1bd608eb129089a30de7eeaffa6dd4c
yuanyue <1140527828@qq.com>
2025-07-31 08:07:49 +0800

    更新DAC tim 触发频率1M

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -698,7 +698,7 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 100-1;
+  htim4.Init.Prescaler = 20-1;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim4.Init.Period = 10-1;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
